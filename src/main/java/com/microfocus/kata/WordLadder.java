5b80b02b1884b8b0a9fd2433938209fe32a79d53
52525f <adavis100@gmail.com>
2016-09-13 16:13:30 -0700

    Add WordLatter solution

---
+++ src/main/java/com/microfocus/kata/WordLadder.java
@@ -0,0 +1,49 @@
+package com.microfocus.kata;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+public class WordLadder {
+    public int ladderLength(String startWord, String endWord, Set<String> wordList) {
+        Queue<String> queue = new LinkedList<>();
+        Map<String, Integer> distMap = new HashMap<>();
+        wordList.add(endWord);
+        queue.add(startWord);
+        distMap.put(startWord, 1);
+
+        while (!queue.isEmpty()) {
+            String current = queue.remove();
+            int currentDist = distMap.get(current);
+
+            for (String next : getNeighbors(current, wordList)) {
+                if (next.equals(endWord)) {
+                    return currentDist + 1;
+                }
+                if (!distMap.containsKey(next)) {
+                    distMap.put(next, currentDist + 1);
+                    queue.add(next);
+                }
+            }
+        }
+        return 0;
+    }
+
+    private List<String> getNeighbors(String s, Set<String> wordList) {
+        List<String> neighbors = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char[] word = s.toCharArray();
+            for (char c='a'; c < 'z'; c++) {
+                word[i] = c;
+                String toCheck = new String(word);
+                if (!toCheck.equals(s) && wordList.contains(toCheck))
+                    neighbors.add(toCheck);
+            }
+        }
+        return neighbors;
+    }
+}
---
+++ src/test/java/com/microfocus/kata/WordLadderTest.java
@@ -0,0 +1,26 @@
+package com.microfocus.kata;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.testng.Assert.assertEquals;
+
+public class WordLadderTest {
+    @DataProvider
+    public static Object[][] provideArgs() {
+        return new Object[][]{
+                new Object[]{"hit", "cog", Stream.of("hot").collect(Collectors.toSet()), 0},
+                new Object[]{"hit", "cog", Stream.of("hot", "dot", "dog", "lot", "log").collect(Collectors.toSet()), 5}
+        };
+    }
+
+    @Test(dataProvider = "provideArgs")
+    public void findsShortestTransformation(String startWord, String endWord, Set<String> wordList, int expectedLength) throws Exception {
+        WordLadder ladder = new WordLadder();
+        assertEquals(ladder.ladderLength(startWord, endWord, wordList), expectedLength);
+    }
+}
